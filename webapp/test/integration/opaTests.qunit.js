8ddc8b10d50924ea453629c34c1c229f4cdaeb0b
CiananNicolai <71702080+CiananNicolai@users.noreply.github.com>
2022-07-13 10:24:51 +0100

    Initial commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["todolist/todolist/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"todolist/todolist/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
