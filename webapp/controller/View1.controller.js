6c3f9493c373854a43aa609e99cab30965266e7a
Cianan Nicolai <cianan.nicolai@sap.com>
2022-08-04 10:23:25 +0000

    Added basic upload ability.

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -634,6 +634,28 @@ function(coreLibrary, Fragment, Controller, DateFormat, JSONModel, unifiedLibrar
 			});
 		},
 
+        handleUploadComplete: function(oEvent) {
+			var sResponse = oEvent.getParameter("response"),
+				iHttpStatusCode = parseInt(/\d{3}/.exec(sResponse)[0]),
+				sMessage;
+
+			if (sResponse) {
+				sMessage = iHttpStatusCode === 200 ? sResponse + " (Upload Success)" : sResponse + " (Upload Error)";
+				MessageToast.show(sMessage);
+			}
+		},
+
+		handleUploadPress: function() {
+			var oFileUploader = this.byId("fileUploader");
+			oFileUploader.checkFileReadable().then(function() {
+				oFileUploader.upload();
+			}, function(error) {
+				MessageToast.show("The file cannot be read. It may have changed.");
+			}).then(function() {
+				oFileUploader.clear();
+			});
+		}
+
         
 	});
 });
